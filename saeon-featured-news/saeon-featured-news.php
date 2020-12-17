c13dffaa28d0883abd3cb99c8719a83acf91a122
Taryn Gloyne <TarynG@saeon.local>
2020-12-17 18:08:25 +0200

    Shortcode for full news list

--- saeon-featured-news/saeon-featured-news.php
+++ saeon-featured-news/saeon-featured-news.php
@@ -43,6 +43,47 @@ function saeon_featured_news()
  }
  add_shortcode( 'saeon-featured-news', 'saeon_featured_news' );
 
+ /* Recent Posts Shortcocde */
+function saeon_news_page()
+{
+
+// the query
+$wpb_all_query = new WP_Query(array('post_type'=>'post', 'post_status'=>'publish', 'posts_per_page'=>-1)); 
+ 
+ if ( $wpb_all_query->have_posts() ) : 
+    $html = "<div class='sn-container'><ul>";
+
+
+	while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); 
+	
+    $html .= "<div class='sn-wrapper sn-newslist'><div class='sn-row'><div class='sn-col-4'>";
+    $html .= "<a  href='" . get_permalink() . "' class='sn-image' "; 
+        
+    if (has_post_thumbnail()) { 
+		
+            $html .= "style='background-image:url(" . get_the_post_thumbnail_url() .")'>"; 
+             } else { 
+
+             } 
+             $html .= "</a></div><div class='sn-col-8'><div><span class='sn-date'>" . get_the_date() . "</span>";
+             $html .= "<h4><a href='" . get_permalink() . "'>" . get_the_title() . "</a></h4>";
+             $html .= "<div class='sn-excerpt'>" . get_the_excerpt() ."</div><a class='btn sn-btn' href='" . get_permalink() ."'>";
+             $html .= "read full article</a></div></div></div></div>";
+
+	endwhile;
+    
+    $html .= "</ul></div>";
+		   
+    wp_reset_postdata();
+ 
+else :
+    $html .= "Sorry, no posts matched your criteria";
+ endif; 
+
+ return $html;
+}
+add_shortcode( 'saeon-news-list', 'saeon_news_page' );
+
 /* Include files */
 function p_support_files() {
     wp_register_style('p_support_files', plugins_url('style.css',__FILE__ ));
@@ -66,8 +107,10 @@ function saeon_featured_news_scripts_page()
     ?>
     <div class="wrap">
         <h2>SAEON Featured News Shortcode</h2>
-        <p>Paste the following shortcode on any page to display latest news</p>
+        <p>Paste the following shortcode on any page to display latest news ites</p>
         <p style="border: 1px solid #ccd0d4;background: #fff;padding: 10px 20px;font-size: 16px;display: inline-block;">[saeon-featured-news]</p>
+        <p style="margin-top:30px">Paste the following shortcode on any page to display full news list</p>
+        <p style="border: 1px solid #ccd0d4;background: #fff;padding: 10px 20px;font-size: 16px;display: inline-block;">[saeon-news-list]</p>
     </div>
 
     <?php
